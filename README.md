d2523a2de20fb6b6aef0d575ea6e06d7a61c86be
kfwong <kfwong@ucdavis.edu>
2018-02-20 14:57:44 -0800

    Redesigned Website
    Content not yet filled but overall layout is complete

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# CSAWebsite
+
--- login_system/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* Database connection settings */
-$host = 'localhost';
-$user = 'root';
-$pass = 'csaucd111';
-$db = 'csa';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
